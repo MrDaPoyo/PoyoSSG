c75bfbc35d602d061879de293703953f1057cbe5
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-08-06 09:54:30 +0000

    working!

--- index.js
+++ index.js
@@ -1,59 +1,20 @@
 console.time('Execution Time');
 const fs = require('fs');
 const path = require('path');
-const interviewProcessor = require('./interviewProcessor');
 const mdProcessor = require('./mdProcessor');
 const ejs = require('ejs');
-const { title } = require('process');
-const marked = require('marked');
-
-const interview_files = fs.readdirSync('src/interviews');
 const post_files = fs.readdirSync('src/posts');
-const interviewsFilePath = path.join(__dirname, '/predist/interviews.json');
-const rawInterviewData = fs.readFileSync(interviewsFilePath, 'utf8');
-const interviewArray = JSON.parse(rawInterviewData).map(post => JSON.parse(post));
 
+const postArray = [];
 
-
-async function processAllInterviews(all_files) {
-  let files = [];
-  all_files.forEach(file => {
-    if (path.extname(file) == ".md") {
-      console.log(file);
-      files.push(file);
-    }
-  })
-
-  for (let i = 0; i < files.length; i++) {
-    const file = files[i];
-    console.log(`Processing ${file}`);
-    const text = fs.readFileSync(`src/interviews/${file}`, 'utf8');
-    const metadata = mdProcessor.extractMetadata(text);
-    const content = text.replace(/^---\n[\s\S]+?\n---/, '').trim();
-    const converted = marked(content);
-    const interview = fs.readFileSync(`src/interviews/${file}`, 'utf8');
-    let interviewName = file.replace('.md', '.html');
-
-    let rendered = await ejs.renderFile('src/templates/interview.ejs', { interview : interviewProcessor.processMarkdown(interview), title: 'Interview' });
-    fs.writeFileSync(`predist/interviews/${interviewName}`, rendered);
-  }
+for (let file of post_files) {
+  postArray.push(file.replace('.md', ''));
 }
 
-function interviewsJsonify(all_files) {
-  let interviews = [];
-  all_files.forEach(file => {
-    if (path.extname(file) == ".md") {
-      const interview = fs.readFileSync(`src/interviews/${file}`, 'utf8');
-      const interviewName = file.replace('.md', '.html');
-      const data = interviewProcessor.readData(interview, interviewName);
-      interviews.push(data);
-    }
-  });
-  fs.writeFileSync('predist/interviews.json', JSON.stringify(interviews));
-}
+fs.writeFileSync('posts.json', JSON.stringify(postArray));
 
 async function mergeEjsFiles() {
-  const index_html = await ejs.renderFile('src/templates/index.ejs', { posts: interviewArray, title: 'Index' });
+  const index_html = await ejs.renderFile('src/templates/index.ejs', { posts: postArray, title: 'Index' });
   fs.writeFileSync('predist/index.html', index_html);
   fs.cpSync('src/templates/static/index.css', 'predist/index.css');
 };
@@ -68,11 +29,7 @@ function checkMDFiles(files) {
   return mdFiles;
 }
 
-
-// transform .md PoyoInterviews to .ejs files
-processAllInterviews(interview_files);
-interviewsJsonify(interview_files);
-mergeEjsFiles(interview_files);
+mergeEjsFiles();
 checkMDFiles(post_files).forEach(file => {
   mdProcessor.processMarkdown(path.join(__dirname, 'src/posts/' + file));
 });
@@ -80,8 +37,8 @@ checkMDFiles(post_files).forEach(file => {
 const files = fs.readdirSync("src/posts").filter(file => path.extname(file) === '.md');
 
 files.forEach(file => {
-    console.log(`Processing ${file}`);
-    mdProcessor.processMarkdown(path.join("src/posts", file));
+  console.log(`Processing ${file}`);
+  mdProcessor.processMarkdown(path.join("src/posts", file));
 });
 
 
---
+++ src/posts/cookies.md
@@ -0,0 +1,10 @@
+---
+author: Poyo!
+title: Why aren't Cookies free for everyone?
+category: "international"
+taxonomies:
+  tags: ["cookies", "international", "philosophy"] 
+---
+
+# Cookies.
+Cookies are awesome things. I love cookies. They should be free for everyone. Cookies are incredibly cheap to make, and they are a great way to make people happy. So, why aren't cookies free for everyone? 
